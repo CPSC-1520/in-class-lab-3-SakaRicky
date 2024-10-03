d0c4b5cc0add984f433fe8a2a884db0274842960
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2024-10-02 21:54:07 -0600

    feat: added the interactivity with js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 // Enter your code below.
+// Enter your code below.
+const javascriptResources = document.querySelector(".javascript-resources");
+const showResourcesBtn = document.querySelector("#show-resources");
+showResourcesBtn.addEventListener('click', () => {
+    javascriptResources.classList.remove("d-none");
+
+});
+
+const listOfItems = document.querySelectorAll(".javascript-resources div");
+listOfItems.forEach(item => {
+    item.addEventListener("mouseover", (event) => {
+        event.target.classList.add("fw-bold")
+    });
+
+    item.addEventListener("mouseout", (event) => {
+        event.target.classList.remove("fw-bold")
+    })
+})
+
+listOfItems.forEach(item => {
+    item.addEventListener("click", (event) => {
+        event.target.classList.add("fst-italic")
+    });
+})
+
